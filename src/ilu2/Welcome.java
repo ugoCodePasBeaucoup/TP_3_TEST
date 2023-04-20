c8b52076ed813b75cb96837322146fef955332f5
ugoCodePasBeaucoup <max.planck.bg@gmail.com>
2023-04-20 17:39:12 +0200

    EX_2

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -3,6 +3,8 @@ package ilu2;
 public class Welcome {
 
 	public static String welcome(String input) {
+		if (input.isBlank())
+			return "Hello, my friend";
 		StringBuilder sb = new StringBuilder("Hello, ");
 		sb.append(input.substring(0, 1).toUpperCase());
 		sb.append(input.substring(1));
--- tests/ilu2/WelcomeTest.java
+++ tests/ilu2/WelcomeTest.java
@@ -17,6 +17,14 @@ class WelcomeTest {
 		assertEquals("Hello, Bob", Welcome.welcome("bob"));
 		assertEquals("Hello, Jack", Welcome.welcome("jack"));
 		assertEquals("Hello, Marc", Welcome.welcome("Marc"));
+		assertEquals("Hello, M", Welcome.welcome("M"));
+	}
+
+	@Test
+	void EX_2() {
+		assertEquals("Hello, my friend", Welcome.welcome(""));
+		assertEquals("Hello, my friend", Welcome.welcome(" "));
+		assertEquals("Hello, my friend", Welcome.welcome("   "));
 	}
 
 }
